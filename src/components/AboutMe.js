5cdce3f8f77e69b6bc95eb14d82bc3855ceeee52
JanviMahajan14 <janvimahajan337@gmail.com>
2020-07-25 19:21:23 +0530

    Minor Fixes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
       <img src="https://res.cloudinary.com/smilingcloud/image/upload/v1595518880/img2_kl5dxd.jpg" alt="" srcset="" width="196px" height="186px" className="img-fluid" />
       <h1 className="me">Who I am</h1>
       <p className="introduction">I am a computer science undergraduate with a strong interest in Problem-Solving and Software Development. I dance, travel and enjoy learning about new technologies. My goal is not only to
-      adopt it and evolve with it but also contribute to the awesome changes it brings to
+      adopt it and evolve with it but also contribute to the awesome changes that technology brings to
       our life! that's why ...</p>
       <span className="Icode">I code!</span>
     </div>
