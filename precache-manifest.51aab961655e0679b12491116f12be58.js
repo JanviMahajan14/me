54b355f467424eb699063d26b7054faa494af537
JanviMahajan14 <janvimahajan337@gmail.com>
2020-07-25 17:23:22 +0530

    Updates

---
+++ precache-manifest.51aab961655e0679b12491116f12be58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbad65307876e404dda0f38b101ad9a9",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "22a498ccc17d15b82696",
+    "url": "/me/static/css/main.c45673e6.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/me/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a498ccc17d15b82696",
+    "url": "/me/static/js/main.72c6d6a1.chunk.js"
+  },
+  {
+    "revision": "580988083407601e9880",
+    "url": "/me/static/js/runtime-main.0e69a599.js"
+  }
+]);
