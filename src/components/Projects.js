d7bd90651cf8a6fe6ab1c90817ab8f75a789a264
JanviMahajan14 <janvimahajan337@gmail.com>
2021-04-09 17:37:49 +0530

    Updated Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,8 +8,8 @@ const Projects = () => {
                 <div className="col-xl-7 col-xs-12"><img className="materialboxed" src="https://res.cloudinary.com/smilingcloud/image/upload/v1595668246/Screenshot_from_2020-07-24_15-56-04_kjw22z.png" alt="" style={{ width: "400px", height: "300px" }}></img></div>
                 <div className="col-xl-5 col-xs-12">
                     <h4 className="project-title">
-                        <a href="https://github.com/JanviMahajan14/Social-Buddy" target="_blank">
-                        Social Buddy
+                        <a href="https://github.com/JanviMahajan14/Smvdu_Blog" target="_blank">
+                        Smvdu_Blog
                         <i class="fa fa-github fa-1x" aria-hidden="true" style={{marginLeft:"10px"}}></i>
                         </a>
                     </h4>
@@ -24,71 +24,67 @@ const Projects = () => {
                     </div>
                     <div className="project-description">
                         <p>
-                            A social-networking Full Stack React Application with React Hooks and Context API for state management.
-                            Peristent No Sql DB and JWT Authentication Features.
-                            Includes RESTful API with Express and NodeJS
+                            A blog website for university built with React Hooks and Context API for state
+                            management. Peristent No Sql DB and JWT Authentication Features.
+                            Includes RESTful API with Express and NodeJS.Have capabilities to like, comment, post and follow, unfollow people.
+                            Feature to upload and download Notes to help university students.
                         </p>
                     </div>
                 </div>
             </div>
 
-            {/* testib */}
-            {/* <div className="row project-content">
-                <div className="col-6"><img className="materialboxed" src="https://res.cloudinary.com/smilingcloud/image/upload/v1595668246/Screenshot_from_2020-07-24_15-56-04_kjw22z.png" alt="" style={{ width: "400px", height: "300px" }}></img></div>
-                <div className="col-6">
+            
+            <div className="row project-content">
+                <div className="col-xl-5 col-xs-12">
                     <h4 className="project-title">
-                        <a href="https://github.com/JanviMahajan14/Social-Buddy" target="_blank">
-                            Social Buddy
+                        <a href="https://github.com/JanviMahajan14/Foodies_Check" target="_blank">
+                            Foodies_Check
                         <i class="fa fa-github fa-1x" aria-hidden="true" style={{ marginLeft: "10px" }}></i>
                         </a>
                     </h4>
                     <div className="project-suite">
+                        <span>NextJS</span>
                         <span>ReactJS</span>
-                        <span>React Hooks</span>
-                        <span>Context API</span>
-                        <span>NodeJS</span>
-                        <span>Express</span>
-                        <span>Mongo DB</span>
-                        <span>JWT Auth</span>
+                        <span>MongoDB Atlas</span>
+                        <span>Payment Gateway</span>
                     </div>
                     <div className="project-description">
                         <p>
-                            A social-networking Full Stack React Application with React Hooks and Context API for state management.
-                            Peristent No Sql DB and JWT Authentication Features.
-                            Includes RESTful API with Express and NodeJS
+                            Foodies Check is a fully functional portal to order your favorite food online and avoid paying in cash.
+                            This project is an initiative to digitalize the nearby food stores at times of COVID-19.
+                            Includes CART functionality, Payment Gateway Integration using STRIPE API.
+                            Feature to support multiple admins to Add and Delete their Dishes.
+                            CRUD application built with NextJS, ReactJs (Frontend), MongoDB (NoSQL DB).
                         </p>
                     </div>
                 </div>
+                <div className="col-xl-7 col-xs-12" style={{ paddingLeft:"110px"}}><img className="materialboxed" src="https://res.cloudinary.com/smilingcloud/image/upload/v1617967715/Screenshot_from_2021-04-09_17-02-28_d7fsdb.png" alt="" style={{ width: "400px", height: "300px" }}></img></div>
             </div>
 
-            {/* testing */}
+            
             {/* <div className="row project-content">
-                <div className="col-6"><img className="materialboxed" src="https://res.cloudinary.com/smilingcloud/image/upload/v1595668246/Screenshot_from_2020-07-24_15-56-04_kjw22z.png" alt="" style={{ width: "400px", height: "300px" }}></img></div>
+                <div className="col-6"><img className="materialboxed" src="https://res.cloudinary.com/smilingcloud/image/upload/v1617967729/Screenshot_from_2021-04-09_16-57-55_p3fytj.png" alt="" style={{ width: "400px", height: "300px" }}></img></div>
                 <div className="col-6">
                     <h4 className="project-title">
-                        <a href="https://github.com/JanviMahajan14/Social-Buddy" target="_blank">
-                            Social Buddy
+                        <a href="https://github.com/JANVIMAHAJAN14/WEATHER-APP" target="_blank">
+                            Weather Application
                         <i class="fa fa-github fa-1x" aria-hidden="true" style={{ marginLeft: "10px" }}></i>
                         </a>
                     </h4>
                     <div className="project-suite">
-                        <span>ReactJS</span>
-                        <span>React Hooks</span>
-                        <span>Context API</span>
                         <span>NodeJS</span>
                         <span>Express</span>
-                        <span>Mongo DB</span>
-                        <span>JWT Auth</span>
+                        <span>handlebars</span>
                     </div>
                     <div className="project-description">
                         <p>
-                            A social-networking Full Stack React Application with React Hooks and Context API for state management.
-                            Peristent No Sql DB and JWT Authentication Features.
-                            Includes RESTful API with Express and NodeJS
+                            Weather application built with NodeJs, express, and handlebars.
+                            It uses Geocoding and Dark Sky API. Provides information regarding the weather of the place as perthe location
+                            entered by the user.
                         </p>
                     </div>
                 </div>
-            </div>  */}
+            </div>   */}
         </div>
     );
 }
